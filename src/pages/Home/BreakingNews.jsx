44c0242092722a544583032bacdf0e56afacd141
Alal Hosen <alalhosen1992620120@gmail.com>
2026-01-17 00:53:29 +0600

    alltime give update time, timezone set from moment.js

---
+++ src/pages/Home/BreakingNews.jsx
@@ -0,0 +1,9 @@
+// const BreakingNews = () => {
+//   return (
+//     <div>
+//       <button className="btn btn-danger">Breaking News</button>
+//     </div>
+//   );
+// };
+
+// export default BreakingNews;
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,12 @@
-import logo from '../../../assets/logo.png';
+import logo from "../../../assets/logo.png";
+import moment from "moment";
 
 const Header = () => {
   return (
-    <div>
-    <img src={logo} alt="" />
+    <div className="text-center">
+      <img className="mx-auto" src={logo} alt="" />
+      <p>Journalism Without Fear or Favour</p>
+      <p className="text-3xl">{moment().format("dddd, MMMM D, YYYY")}</p>
     </div>
   );
 };
