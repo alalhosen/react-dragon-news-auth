ee915dc869d84e2b5c2f8ce1c3a613ec2d03ca84
Alal Hosen <alalhosen1992620120@gmail.com>
2026-01-16 19:46:33 +0600

    set font poppins

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h2 className="text-3xl">This is Home</h2>
+      <h2 className="text-3xl font-poppins font-bold">This is Home</h2>
     </div>
   );
 };
