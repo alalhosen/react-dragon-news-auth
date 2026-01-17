d3babd8812eab4f13edf0d9856d93dd31747557c
Alal Hosen <alalhosen1992620120@gmail.com>
2026-01-17 16:45:09 +0600

    install react marquee

--- src/pages/Home/BreakingNews.jsx
+++ src/pages/Home/BreakingNews.jsx
@@ -1,9 +1,15 @@
-// const BreakingNews = () => {
-//   return (
-//     <div>
-//       <button className="btn btn-danger">Breaking News</button>
-//     </div>
-//   );
-// };
+import Marquee from "react-fast-marquee";
 
-// export default BreakingNews;
+const BreakingNews = () => {
+  return (
+    <div>
+      <button className="btn btn-secondary">Breaking News</button>
+      <Marquee>
+        I can be a React component, multiple React components, or just some
+        text.
+      </Marquee>
+    </div>
+  );
+};
+
+export default BreakingNews;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import Header from "../Shared/Header/Header";
 import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
 import Navbar from "../Shared/Navbar/Navbar";
 import RightSideNav from "../Shared/RightSideNav/RightSideNav";
+import BreakingNews from "./BreakingNews";
 
 const Home = () => {
   return (
     <div>
       <Header></Header>
+      <BreakingNews></BreakingNews>
       <Navbar></Navbar>
       <h2 className="text-3xl font-poppins font-bold">This is Home</h2>
       <div className="text-3xl grid grid-cols-1 md:grid-cols-4 gap-6">
