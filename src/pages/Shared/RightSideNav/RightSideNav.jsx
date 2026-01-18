d5b7cf3c93c96bc92338073668f07695c6061938
Alal Hosen <alalhosen1992620120@gmail.com>
2026-01-18 10:41:03 +0600

    image set & import in QZone section

--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,19 +1,61 @@
+import { FaFacebook, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
 import { GrGoogle } from "react-icons/gr";
+import QZone1 from '../../../assets/QZone1.png'
+import QZone2 from '../../../assets/QZone2.png'
+import QZone3 from '../../../assets/QZone3.png'
 
 const RightSideNav = () => {
   return (
     <div>
-      <div>
-        <button className="btn">
+      {/* Login With */}
+      <div className="p-4 space-y-3 mb-6">
+        <h2 className="text-2xl font-bold">Login With</h2>
+        <button className="btn btn-outline w-full">
           <GrGoogle></GrGoogle>
-          Button
+          Login With Google
+        </button>
+        <button className="btn btn-outline w-full">
+          <FaGithub></FaGithub>
+          Login With Github
         </button>
       </div>
-      <div>
-        <button className="btn">
-          <GrGoogle></GrGoogle>
-          Button
-        </button>
+
+      {/* Find Us on */}
+      <div className="p-4  mb-6">
+        <h2 className="text-2xl mb-3  font-bold">Find Us On</h2>
+        <a
+          className="p-4 flex text-lg items-center border rounded-t-lg"
+          href=""
+        >
+          <FaFacebook
+            className="mr-3
+          "
+          ></FaFacebook>
+          <span>Facebook</span>
+        </a>
+        <a
+          className="p-4 flex text-lg items-center border-x"
+          href=""
+        >
+          <FaTwitter className="mr-3"></FaTwitter>
+          <span>Twitter</span>
+        </a>
+        <a
+          className="p-4 flex text-lg items-center border rounded-b-lg"
+          href=""
+        >
+          <FaInstagram className="mr-3"></FaInstagram>
+          <span>Instagram</span>
+        </a>
+      </div>
+
+      {/* Q Zone */}
+      <div className="p-4 space-y-3 mb-6">
+        <h2 className="text-2xl font-bold">Q Zone</h2>
+        <img src={QZone1} alt="" />
+        <img src={QZone2} alt="" />
+        <img src={QZone3} alt="" />
+       
       </div>
     </div>
   );
