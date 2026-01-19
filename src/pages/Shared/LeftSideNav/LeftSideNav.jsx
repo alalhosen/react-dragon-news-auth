b879934da341bad0d3a7d1eadfe9175caa9927ab
Alal Hosen <alalhosen1992620120@gmail.com>
2026-01-19 22:26:38 +0600

    use fetch in leftSideNav

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,15 +1,26 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    fatch("categories.json");
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
   }, []);
 
   return (
-    <div>
-      <h2 className="text-2xl">LeftSideNav</h2>
+    <div className="space-y-2">
+      <h2 className="text-2xl font-bold">All Categories</h2>
+      {
+        categories.map(category=><Link 
+            className="block ml-4 text-xl font-semibold"
+             key={category.id}
+             to={`/category/${category.id }`}
+             >{category.name}
+             </Link>)
+      }
     </div>
   );
 };
