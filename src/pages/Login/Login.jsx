97f14cd2fceb5e9d19a6c484cdff4c56f7c6d72d
Alal Hosen <alalhosen1992620120@gmail.com>
2026-01-21 21:46:29 +0600

    login component set

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,44 @@
+import Navbar from "../Shared/Navbar/Navbar";
 
 const Login = () => {
-    return (
-        <div>
-<h2 className="text-3xl">This in Login.</h2>            
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h2 className="text-3xl">This is Login</h2>
+      <form className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            placeholder="email"
+            className="input input-bordered"
+            required
+          />
         </div>
-    );
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type="password"
+            placeholder="password"
+            className="input input-bordered"
+            required
+          />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">
+              Forgot password?
+            </a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
