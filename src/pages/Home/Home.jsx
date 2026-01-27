563504f0498a9b310a711bf8335ebb1a2c2a9eb6
Alal Hosen <alalhosen1992620120@gmail.com>
2026-01-27 23:58:01 +0600

    create a new component NewsCard

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
+import { useLoaderData } from "react-router-dom";
 import Header from "../Shared/Header/Header";
 import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
 import Navbar from "../Shared/Navbar/Navbar";
 import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 import BreakingNews from "./BreakingNews";
+import NewsCard from "./NewsCard";
 
 const Home = () => {
+  const news=useLoaderData();
+  console.log(news)
   return (
     <div>
       <Header></Header>
@@ -16,6 +20,9 @@ const Home = () => {
           <LeftSideNav></LeftSideNav>
         </div>
         <div className="md:col-span-2 p-2 border">
+          {
+            news.map(aNews=><NewsCard key={}></NewsCard>)
+          }
           <h2 className="text-3xl">News Coming Soon....</h2>
         </div>
         <div className="border p-2">
