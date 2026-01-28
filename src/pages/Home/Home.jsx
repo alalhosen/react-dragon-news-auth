a8581603f479c71304e97f193d03e340f84b791a
Alal Hosen <alalhosen1992620120@gmail.com>
2026-01-28 12:31:59 +0600

    newsCare details & title set

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,8 +7,8 @@ import BreakingNews from "./BreakingNews";
 import NewsCard from "./NewsCard";
 
 const Home = () => {
-  const news=useLoaderData();
-  console.log(news)
+  const news = useLoaderData();
+  console.log(news);
   return (
     <div>
       <Header></Header>
@@ -16,16 +16,17 @@ const Home = () => {
       <Navbar></Navbar>
       <h2 className="text-3xl font-poppins font-bold">This is Home</h2>
       <div className="text-3xl grid grid-cols-1 md:grid-cols-4 gap-6">
-        <div className="border p-2">
+        <div>
           <LeftSideNav></LeftSideNav>
         </div>
-        <div className="md:col-span-2 p-2 border">
-          {
-            news.map(aNews=><NewsCard key={}></NewsCard>)
-          }
-          <h2 className="text-3xl">News Coming Soon....</h2>
+        
+        {/* news card container */}
+        <div className="md:col-span-2 p-2">
+          {news.map((aNews) => (
+            <NewsCard key={aNews._id} news={aNews}></NewsCard>
+          ))}
         </div>
-        <div className="border p-2">
+        <div>
           <RightSideNav></RightSideNav>
         </div>
       </div>
--- src/pages/Home/NewsCard.jsx
+++ src/pages/Home/NewsCard.jsx
@@ -1,10 +1,22 @@
+import { Link } from "react-router-dom";
 
-const NewsCard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const NewsCard = ({ news }) => {
+  const { title, thumbnail_url,details } = news;
+  return (
+    <div className="card bg-base-100 shadow-xl mb-16">
+      <figure>
+        <img src={thumbnail_url} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2>
+        {
+            details.length>200 
+            ?<p>{details.slice(0,200)} <Link className="text-blue-500">Read More...</Link></p>
+            :<p>{details}</p>
+        }
+      </div>
+    </div>
+  );
 };
 
 export default NewsCard;
