c0b81f6c3dcb7e3e6324e862c77f45b967c6eccc
Alal Hosen <alalhosen1992620120@gmail.com>
2026-02-04 22:55:33 +0600

    firebase install & deploy and others function set up

--- src/pages/Home/BreakingNews.jsx
+++ src/pages/Home/BreakingNews.jsx
@@ -6,14 +6,17 @@ const BreakingNews = () => {
     <div className="flex">
       <button className="btn btn-secondary">Breaking News</button>
       <Marquee pauseOnHover={true} speed={100}>
-        <Link className="mr-12" to='/'>
+        <Link className="mr-12 bg-red-50 p-2
+        " to='/'>
           I can be a React component, multiple React components.....
         </Link>
-        <Link className="mr-12" to='/'>
-          I can be a React component, multiple React components.....
+        <Link className="mr-12 bg-red-50 p-2
+        " to='/'>
+          Internet Services Disrupted Across the Country, Causing Public Inconvenience.....
         </Link>
-        <Link className="mr-12" to='/'>
-          I can be a React component, multiple React components.....
+        <Link className="mr-12 bg-red-50 p-2
+        " to='/'>
+         The Dragon News is in the best news all over the world.....
         </Link>
       </Marquee>
     </div>
